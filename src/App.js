db18315a6e44a2b8141667b7529c85c42b094b6a
eddymandran <eddy.mandran@gmail.com>
2022-11-03 15:36:54 +0100

    Teams done

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { ColorModeContext, useMode } from "./theme";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-/* import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
+import Team from "./scenes/team";
+/* import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import Bar from "./scenes/bar";
 import Form from "./scenes/form";
@@ -29,8 +29,8 @@ function App() {
             <Topbar/>
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
+               <Route path="/team" element={<Team />} />
+              {/*<Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
