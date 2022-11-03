a780f00c554b324d54c70121e2a6bb7a07b9988b
eddymandran <eddy.mandran@gmail.com>
2022-11-03 11:35:48 +0100

    Theme with light and dark mode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-function App() {
-  return (
-    <div className="app">
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
-    </div>
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      <div className="app">
+        <main className='content'></main>
+      </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
