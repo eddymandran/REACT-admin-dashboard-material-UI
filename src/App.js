495386fd2537a4b85a67d973f452d88adeb3993b
eddymandran <eddy.mandran@gmail.com>
2022-11-03 12:36:41 +0100

    Top bar done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from './scenes/global/Topbar';
+import Dashboard from './scenes/dashboard/';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -9,7 +11,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
       <div className="app">
-        <main className='content'></main>
+        <main className='content'>
+          <Topbar />
+        </main>
       </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
