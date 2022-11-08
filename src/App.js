c2bdc49baf3a23f902c6ec3ce9de6ab91ec54eea
eddymandran <eddy.mandran@gmail.com>
2022-11-08 11:56:53 +0100

    Invoices done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./theme";
+import {Route, Routes} from "react-router-dom";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import {ColorModeContext, useMode} from "./theme";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
-/* import Invoices from "./scenes/invoices";*/
+import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
+
 /*import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 import Line from "./scenes/line";
@@ -31,8 +31,8 @@ function App() {
               <Route path="/" element={<Dashboard />} />
                <Route path="/team" element={<Team />} />
              <Route path="/contacts" element={<Contacts />} />
-              {/*<Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
+              <Route path="/invoices" element={<Invoices />} />
+                {/*<Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
